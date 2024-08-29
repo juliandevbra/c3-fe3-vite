fd563378620ebf12e24b0388408623ef0fed1567
unknown <DH@DH-CARRERA-34.DH.digitalhouse.com>
2024-08-29 15:17:07 -0300

    Clase 12: Testing con vitest

---
+++ src/Components/LoginForm.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const LoginForm = ({ handleClick }) => {
+  return (
+    <div>
+      <h1
+      //   style={{ display: "none" }}
+      >
+        Debe ingresar
+      </h1>
+      <input type="email" role="email" />
+      <input type="password" data-testid="password" />
+      <button onClick={handleClick}>Enviar</button>
+    </div>
+  );
+};
+
+export default LoginForm;
